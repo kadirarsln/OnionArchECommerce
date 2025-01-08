0019f2c1eb0dcc9a726e0728ef4f07a39cf24001
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2025-01-09 01:03:22 +0300

    Repositories updated

---
+++ src/project/ECommerce.Application/Services/Repositories/ITagRepository.cs
@@ -0,0 +1,8 @@
+﻿using Core.Persistence.Repositories;
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.Application.Services.Repositories;
+
+public interface ITagRepository : IAsyncRepository<Tag, Guid>
+{
+}
